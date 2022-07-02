de697481bde8e22e459d654465e597bfc9e9ace5
Belen-Machena <machenabelen@gmail.com>
2022-07-02 04:57:31 -0300

    comienzodelSprint

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,1 +1,40 @@
-#pruebo el PR a ver si funciona
+from datetime import datetime
+from sqlite3 import Timestamp
+import sys
+import csv
+parametros = sys.argv
+
+if len (parametros) >1:
+    dni = parametros[1]
+    salida= parametros[2]
+    tipo_cheque = parametros[3]
+
+print(sys.argv)
+
+archivo= open('test.csv', 'r') # abre el archivo
+lineas = csv.reader(archivo) # lee el archivo
+
+
+for linea in lineas:
+    if linea[-3] == dni:
+        if linea[1] == tipo_cheque:
+            print(linea[-2])
+            break
+    else:
+        print("No existe el DNI")
+        break
+
+archivo.close
+
+if salida =='CSV':
+    TIMESTAMPSACTUAL = str(datetime.now().timestamp())
+    ar = open (dni + TIMESTAMPSACTUAL + '.csv', 'a')
+    archivoCSV = csv.writer(ar)
+    archivoCSV.writerow(linea)
+    ar.close()
+elif salida == 'TXT':
+    ar = open (dni + '.txt', 'a')
+    archivoTXT = csv.writer(ar)
+    archivoTXT.writerow(linea)
+    ar.close()
+    
